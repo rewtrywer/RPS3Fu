6287ab56b963d2428372b4e8faf5c53cfaa86332
Дыня Орехов <danaran319@gmail.com>
2024-03-16 17:49:41 +0300

    222

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -26,8 +26,7 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
+        private void InitializeComponent() {
             formsPlot1 = new ScottPlot.WinForms.FormsPlot();
             button1 = new Button();
             dataGridView1 = new DataGridView();
@@ -49,15 +48,17 @@
             // 
             formsPlot1.DisplayScale = 1.25F;
             formsPlot1.Location = new Point(-1, 0);
+            formsPlot1.Margin = new Padding(3, 2, 3, 2);
             formsPlot1.Name = "formsPlot1";
-            formsPlot1.Size = new Size(508, 358);
+            formsPlot1.Size = new Size(444, 268);
             formsPlot1.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(524, 318);
+            button1.Location = new Point(458, 238);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(124, 40);
+            button1.Size = new Size(108, 30);
             button1.TabIndex = 1;
             button1.Text = "Построить";
             button1.UseVisualStyleBackColor = true;
@@ -67,10 +68,13 @@
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { X, YPos, YNeg });
-            dataGridView1.Location = new Point(524, 32);
+            dataGridView1.Location = new Point(458, 24);
+            dataGridView1.Margin = new Padding(3, 2, 3, 2);
             dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersVisible = false;
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(278, 268);
+            dataGridView1.Size = new Size(229, 201);
             dataGridView1.TabIndex = 2;
             // 
             // X
@@ -99,59 +103,65 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(109, 374);
+            textBox1.Location = new Point(95, 280);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
             textBox1.PlaceholderText = "a";
-            textBox1.Size = new Size(34, 27);
+            textBox1.Size = new Size(30, 23);
             textBox1.TabIndex = 3;
             textBox1.TextAlign = HorizontalAlignment.Center;
             textBox1.KeyPress += textBox2_KeyPress;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(167, 374);
+            textBox2.Location = new Point(146, 280);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
             textBox2.Name = "textBox2";
             textBox2.PlaceholderText = "c";
-            textBox2.Size = new Size(34, 27);
+            textBox2.Size = new Size(30, 23);
             textBox2.TabIndex = 4;
             textBox2.TextAlign = HorizontalAlignment.Center;
             textBox2.KeyPress += textBox2_KeyPress;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(227, 374);
+            textBox3.Location = new Point(199, 280);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
             textBox3.Name = "textBox3";
             textBox3.PlaceholderText = "шаг";
-            textBox3.Size = new Size(34, 27);
+            textBox3.Size = new Size(30, 23);
             textBox3.TabIndex = 5;
             textBox3.TextAlign = HorizontalAlignment.Center;
             textBox3.KeyPress += textBox3_KeyPress;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(290, 374);
+            textBox4.Location = new Point(254, 280);
+            textBox4.Margin = new Padding(3, 2, 3, 2);
             textBox4.Name = "textBox4";
             textBox4.PlaceholderText = "min";
-            textBox4.Size = new Size(34, 27);
+            textBox4.Size = new Size(30, 23);
             textBox4.TabIndex = 6;
             textBox4.TextAlign = HorizontalAlignment.Center;
             textBox4.KeyPress += textBox2_KeyPress;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(350, 374);
+            textBox5.Location = new Point(306, 280);
+            textBox5.Margin = new Padding(3, 2, 3, 2);
             textBox5.Name = "textBox5";
             textBox5.PlaceholderText = "max";
-            textBox5.Size = new Size(34, 27);
+            textBox5.Size = new Size(30, 23);
             textBox5.TabIndex = 7;
             textBox5.TextAlign = HorizontalAlignment.Center;
             textBox5.KeyPress += textBox2_KeyPress;
             // 
             // button2
             // 
-            button2.Location = new Point(524, 378);
+            button2.Location = new Point(458, 284);
+            button2.Margin = new Padding(3, 2, 3, 2);
             button2.Name = "button2";
-            button2.Size = new Size(124, 40);
+            button2.Size = new Size(108, 30);
             button2.TabIndex = 8;
             button2.Text = "Сохранить";
             button2.UseVisualStyleBackColor = true;
@@ -159,9 +169,10 @@
             // 
             // button3
             // 
-            button3.Location = new Point(676, 378);
+            button3.Location = new Point(592, 284);
+            button3.Margin = new Padding(3, 2, 3, 2);
             button3.Name = "button3";
-            button3.Size = new Size(124, 40);
+            button3.Size = new Size(108, 30);
             button3.TabIndex = 9;
             button3.Text = "Загрузить";
             button3.UseVisualStyleBackColor = true;
@@ -169,9 +180,10 @@
             // 
             // button4
             // 
-            button4.Location = new Point(676, 318);
+            button4.Location = new Point(592, 238);
+            button4.Margin = new Padding(3, 2, 3, 2);
             button4.Name = "button4";
-            button4.Size = new Size(124, 40);
+            button4.Size = new Size(108, 30);
             button4.TabIndex = 10;
             button4.Text = "Информация";
             button4.UseVisualStyleBackColor = true;
@@ -179,9 +191,9 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(860, 450);
+            ClientSize = new Size(752, 338);
             Controls.Add(button4);
             Controls.Add(button3);
             Controls.Add(button2);
@@ -193,6 +205,7 @@
             Controls.Add(dataGridView1);
             Controls.Add(button1);
             Controls.Add(formsPlot1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Овал Кассини";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,21 @@ namespace рпс3
             button2.Enabled = false;
         }
 
+        public static int CountDigitsAfterDecimal(double number) {
+            // Convert the number to a string
+            string numberStr = number.ToString(System.Globalization.CultureInfo.InvariantCulture);
+            // Find the decimal point
+            int decimalPointIndex = numberStr.IndexOf('.');
+
+            // If there is no decimal point, return 0
+            if (decimalPointIndex == -1) {
+                return 0;
+            }
+
+            // Count the number of digits after the decimal point
+            return numberStr.Length - decimalPointIndex - 1;
+        }
+
         private void Graph()
         {
             ScottPlot.Plot myPlot = formsPlot1.Plot;
@@ -31,11 +46,12 @@ namespace рпс3
                 double a = double.Parse(textBox1.Text);
                 double c = double.Parse(textBox2.Text);
                 double step = double.Parse(textBox3.Text);
+                int digitsAfterDecimal = CountDigitsAfterDecimal(step);
                 double min = double.Parse(textBox4.Text);
                 double max = double.Parse(textBox5.Text);
 
                 myPlot.Clear();
-
+                dataGridView1.Rows.Clear();
                 if (min >= max)
                 {
                     MessageBox.Show("Левая граница должна быть меньше правой");
@@ -55,18 +71,35 @@ namespace рпс3
 
                             if ((Math.Sqrt(Math.Pow(a, 4) + 4 * Math.Pow(c, 2) * Math.Pow(x, 2)) >= 0 && (Math.Sqrt(Math.Pow(a, 4) + 4 * Math.Pow(c, 2) * Math.Pow(x, 2)) - Math.Pow(x, 2) - Math.Pow(c, 2) >= 0)))
                             {
+                                if(X.Count > 0) {
+                                    if(Math.Round(Math.Abs(x-X.Last()), digitsAfterDecimal) > step) {
+                                        var scatter = myPlot.Add.Scatter(X.ToArray(), YPos.ToArray());
+                                        var scatter2 = myPlot.Add.Scatter(X.ToArray(), YNeg.ToArray());
+                                        scatter.MarkerSize = 0;
+                                        scatter2.MarkerSize = 0;
+                                        X.Clear();
+                                        YPos.Clear();
+                                        YNeg.Clear();
+                                        myPlot = formsPlot1.Plot;
+                                    }
+                                }
+
+
                                 YPos.Add(yPos);
                                 YNeg.Add(yNeg);
                                 X.Add(x);
 
-                                dataGridView1.Rows.Add(x, yPos, yNeg);
+                                dataGridView1.Rows.Add(Math.Round(x, digitsAfterDecimal), Math.Round(yPos, digitsAfterDecimal), Math.Round(yNeg, digitsAfterDecimal));
                             }
                         }
+                        if(X.Count > 0) {
+                            var scatter = myPlot.Add.Scatter(X.ToArray(), YPos.ToArray());
+                            var scatter2 = myPlot.Add.Scatter(X.ToArray(), YNeg.ToArray());
+                            scatter.MarkerSize = 0;
+                            scatter2.MarkerSize = 0;
+                        }
 
-                        var scatter = myPlot.Add.Scatter(X.ToArray(), YPos.ToArray());
-                        var scatter2 = myPlot.Add.Scatter(X.ToArray(), YNeg.ToArray());
-                        scatter.MarkerSize = 0;
-                        scatter2.MarkerSize = 0;
+
                         formsPlot1.Refresh();
                         button2.Enabled = true;
                     }
